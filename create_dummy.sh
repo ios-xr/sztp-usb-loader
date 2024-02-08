4cbc1a0e0805669f75a92cc607b3828a242f230e
Athul M A <athma@cisco.com>
2024-02-08 16:45:12 +0530

    Edit dummy script

--- create_dummy.sh
+++ create_dummy.sh
@@ -21,7 +21,7 @@ python3 usb.py \
         -psc testdata/post_config_script.sh \
         -ch merge \
         -iu dummy_usb/images/image.iso \
-        -ip images/image.iso \
+        -ip images/ \
         -ia sha-256 \
         -oc certificates/owner.cert \
         -ocpk certificates/owner.key \
